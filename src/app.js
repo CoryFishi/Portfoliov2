d86c5742147da8e10408b40f8584b3ae45d28b45
Cory Fishburn <coryjr2002@gmail.com>
2023-05-31 16:00:58 -0700

    Updated

--- src/app.js
+++ src/app.js
@@ -1,24 +1,26 @@
-//const homeButton = document.getElementById("home-button");
-// const aboutButton = document.getElementById("about-button");
-// const eventButton = document.getElementById("projects-button");
-// const contactButton = document.getElementById("contact-button");
-// const homeSection = document.getElementById("home");
-// const aboutSection = document.getElementById("about");
-// const projectsSection = document.getElementById("projects");
-// const contactSection = document.getElementById("contact");
-// homeButton.addEventListener("click", function() {
-//     homeSection.scrollIntoView({behavior: "smooth"});
-// });
-// aboutButton.addEventListener("click", function() {
-//     aboutSection.scrollIntoView({behavior: "smooth"});
-// });
-// projectsButton.addEventListener("click", function() {
-//     projectsSection.scrollIntoView({behavior: "smooth"});
-// });
-// contactButton.addEventListener("click", function() {    
-//     contactSection.scrollIntoView({behavior: "smooth"});
-// });
+//Scroll menu to buttons
+const homeButton = document.getElementById("home-button");
+const aboutButton = document.getElementById("about-button");
+const projectsButton = document.getElementById("projects-button");
+const contactButton = document.getElementById("contact-button");
+const homeSection = document.getElementById("home");
+const aboutSection = document.getElementById("about");
+const projectsSection = document.getElementById("projects");
+const contactSection = document.getElementById("contact");
+homeButton.addEventListener("click", function() {
+    homeSection.scrollIntoView({behavior: "smooth"});
+});
+aboutButton.addEventListener("click", function() {
+    aboutSection.scrollIntoView({behavior: "smooth"});
+});
+projectsButton.addEventListener("click", function() {
+    projectsSection.scrollIntoView({behavior: "smooth"});
+});
+contactButton.addEventListener("click", function() {    
+    contactSection.scrollIntoView({behavior: "smooth"});
+});
 
+//animate sections
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
@@ -32,5 +34,15 @@ const observer = new IntersectionObserver((entries) => {
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
 
+//Scoll to top on refresh
 history.scrollRestoration = "manual"
 
+//SideMenu
+const toggle = document.getElementById("sidepanel");
+function openNav() {
+    if (toggle.style.display=="block") {
+        toggle.style.display = "none"
+    } else {
+        toggle.style.display = "block";
+    }
+}
