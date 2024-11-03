00d5a76d61a1199f37c1b8bb79eadc0bba1eb77a
Cory Fishburn <coryjr2002@gmail.com>
2024-11-03 01:07:23 -0800

    test deploy for netlify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ export default function Navbar({ toggleDarkMode, darkMode }) {
             to="/"
             className="hover:bg-slate-100 dark:hover:bg-gray-700 px-3 py-2 rounded-md text-md font-mediums"
           >
-            Contact
+            Contact123123
           </Link>
         </div>
       </div>
