e838a59b8d48bd63d221a138184bb2bcbd53df9e
Cory Fishburn <coryjr2002@gmail.com>
2024-11-04 13:34:04 -0700

    Projects slider

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -18,11 +18,44 @@ export default function ProjectsSection({ projectsRef }) {
       className="w-full flex flex-col items-center justify-center text-center bg-blue-50 font-roboto"
     >
       <div>
-        <h1 className="text-5xl">My Projects</h1>
+        <h1 className="text-5xl mt-10">My Projects</h1>
       </div>
-      <div className="w-full flex justify-evenly">
-        <div className="w-1/2 grid grid-cols-2 m-3 rounded text-center p-2">
-          <div className="group hover:bg-gray-400 p-4 m-3 hover:scale-105 bg-gray-200 rounded transition duration-300 ease-in-out transform select-none">
+      <div className="w-full flex flex-col justify-evenly">
+        <div className="group w-full flex justify-evenly mx-36">
+          <div className="group w-1/2 grid place-items-center m-3 rounded text-center p-2 transform">
+            <img
+              src={project2Static}
+              alt="Default Image"
+              className="group-hover:opacity-0 transition-opacity duration-300 w-full"
+            />
+            <img
+              src="https://camo.githubusercontent.com/f55bac5a5ea894c90eede9cd223cbaec2bedfaafdc684c3223ee3887c17cabef/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f76312e59326c6b505463354d4749334e6a45785a586c6e4e3239685a444d3063325a684d57566c59335578596d526e6232316f64545a774e7a4a6c626d356f626d5a6e6558493563695a6c634431324d563970626e526c636d35686246396e61575a66596e6c666157516d593351395a772f383378416b6952615a5457486a336d565a392f67697068792e676966"
+              alt="Hover Image"
+              className="absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity duration-300 w-full"
+            />
+          </div>
+
+          <div className="w-1/2 flex justify-center items-center">
+            <div className="w-full p-2 m-3 rounded text-left mx-auto">
+              <h2 className="text-2xl font-bold">
+                Property Management Solution in React
+              </h2>
+              <p className="text-xl max-w-[550px]">
+                This is a solution that allows you to manage your self storage
+                property by connecting to OpenTech's REST APIs, made with
+                vanilla JavaScript and React.
+              </p>
+              <div className="flex text-5xl">
+                <IoLogoJavascript className="text-yellow-500" />
+                <IoLogoCss3 className="text-blue-500" />
+                <IoLogoGithub className="text-black" />
+                <IoLogoHtml5 className="text-orange-500" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full grid grid-cols-4 gap-2 rounded text-center p-2">
+          <div className="group hover:bg-gray-400 p-4 m-3 hover:scale-105 rounded transition duration-300 ease-in-out transform select-none hover:cursor-pointer">
             <div className="flex items-center justify-center relative">
               <img
                 src={project1Static}
@@ -36,7 +69,7 @@ export default function ProjectsSection({ projectsRef }) {
               />
             </div>
             <h2 className="font-bold m-1 text-xl">
-              Property Management Solution in React
+              Property Management Solution
             </h2>
             <div className="flex justify-center items-center text-center w-full space-x-4 text-3xl">
               <RiTailwindCssFill className="text-blue-300" />
@@ -45,7 +78,7 @@ export default function ProjectsSection({ projectsRef }) {
               <IoLogoGithub className="text-black" />
             </div>
           </div>
-          <div className="group hover:bg-gray-400 p-4 m-3 hover:scale-105 bg-gray-200 rounded transition duration-300 ease-in-out transform select-none">
+          <div className="group hover:bg-gray-400 p-4 m-3 hover:scale-105 rounded transition duration-300 ease-in-out transform select-none hover:cursor-pointer">
             <div className="flex items-center justify-center relative">
               <img
                 src={project2Static}
@@ -68,7 +101,7 @@ export default function ProjectsSection({ projectsRef }) {
               <IoLogoHtml5 className="text-orange-500" />
             </div>
           </div>
-          <div className="group hover:bg-gray-400 p-4 m-3 hover:scale-105 bg-gray-200 rounded transition duration-300 ease-in-out transform select-none">
+          <div className="group hover:bg-gray-400 p-4 m-3 hover:scale-105 rounded transition duration-300 ease-in-out transform select-none hover:cursor-pointer">
             <div className="flex items-center justify-center relative">
               <img
                 src={project2Static}
@@ -91,7 +124,7 @@ export default function ProjectsSection({ projectsRef }) {
               <IoLogoHtml5 className="text-orange-500" />
             </div>
           </div>
-          <div className="group hover:bg-gray-400 p-4 m-3 hover:scale-105 bg-gray-200 rounded transition duration-300 ease-in-out transform select-none">
+          <div className="group hover:bg-gray-400 p-4 m-3 hover:scale-105rounded transition duration-300 ease-in-out transform select-none hover:cursor-pointer">
             <div className="flex items-center justify-center relative">
               <img
                 src={project2Static}
@@ -108,7 +141,6 @@ export default function ProjectsSection({ projectsRef }) {
             <div className="flex justify-center items-center text-center w-full space-x-4 text-3xl"></div>
           </div>
         </div>
-        <div className="w-1/2 bg-green-300 p-2 m-3 rounded">s</div>
       </div>
       <div className="group w-full flex justify-evenly">
         <div className="group w-1/2 grid place-items-center m-3 rounded text-center p-2 transform">
